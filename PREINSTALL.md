9a2cf7321b92711bc746f82216f42119dbc06e87
Venkatesh Shanbhag <91714892+theshanbhag@users.noreply.github.com>
2024-12-10 17:30:30 +0530

    inital commit

---
+++ PREINSTALL.md
@@ -0,0 +1,40 @@
+<!-- 
+This file provides your users an overview of your extension. All content is optional, but this is the recommended format. Your users will see the contents of this file when they run the `firebase ext:info` command.
+
+Include any important functional details as well as a brief description for any additional setup required by the user (both pre- and post-installation).
+
+Learn more about writing a PREINSTALL.md file in the docs:
+https://firebase.google.com/docs/extensions/publishers/user-documentation#writing-preinstall
+-->
+
+# MongoDB Atlas Extension
+
+This firebase extension is a v1 for MongoDB Atlas. It can perform the following operations
+
+    1. /findOne
+        * requires user to pass filter in the format {"filter":{"name":"sample"}} 
+    2. /insertOne
+        * requires the user to pass document in JSON format.
+    3. /vectorSearch
+        * requires the user to pass the query in the format {"query": "Tell me about Firebase extensions"}
+
+### The parameters that can be configures at the time of installation are below:
+ * Connection String: The Mongodb SRV string from Atlas -> connect
+ * Database Name: name of the database
+ * Collection Name: Name of the collection you want to connect to.
+ * Embedding Model Name
+ * LLM Model Name
+ * Index Name
+ * Vector Index Name
+ * Vector Field Name
+ * Vector return Name
+
+<!-- We recommend keeping the following section to explain how billing for Firebase Extensions works -->
+# Billing
+
+This extension uses other Firebase or Google Cloud Platform services which may have associated charges:
+
+<!-- List all products the extension interacts with -->
+- Cloud Functions
+
+When you use Firebase Extensions, you're only charged for the underlying resources that you use. A paid-tier billing plan is only required if the extension uses a service that requires a paid-tier plan, for example calling to a Google Cloud Platform API or making outbound network requests to non-Google services. All Firebase services offer a free tier of usage. [Learn more about Firebase billing.](https://firebase.google.com/pricing)
---
+++ functions/index.js
@@ -0,0 +1,126 @@
+import { MongoClient } from 'mongodb';
+import { initializeApp } from "firebase-admin/app";
+import { https } from 'firebase-functions/v1';
+import { VertexAIEmbeddings, ChatVertexAI } from "@langchain/google-vertexai";
+
+import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
+
+
+const app = initializeApp();
+
+
+export const mongodbcrud = https.onRequest(async (req, res) => {
+
+  const route = req.path; 
+  const payload = req.body;
+  const client = new MongoClient(process.env.CONNECTION_STRING);
+  const db = client.db(process.env.DATABASE_NAME);
+  const collection = db.collection(process.env.COLLECTION_NAME);
+
+  const llm = new ChatVertexAI({
+    model: process.env.LLM_MODEL_NAME,
+    temperature: 0.2,
+    maxRetries: 2,
+    // For web, authOptions.credentials
+    // authOptions: { ... }
+    // other params...
+  });
+  const embeddings = new VertexAIEmbeddings({
+    model: process.env.EMBEDDING_MODEL_NAME,
+    location: process.env.LOCATION,
+    // ...
+  });
+
+  const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
+    collection: collection,
+    indexName: process.env.INDEX_NAME, // The name of the Atlas search index. Defaults to "default"
+    textKey: process.env.INDEX_FIELD, // The name of the collection field containing the raw content. Defaults to "text"
+    embeddingKey: process.env.EMBEDDING_FIELD, // The name of the collection field containing the embedded text. Defaults to "embedding"
+  });
+  
+  try {
+    console.log('Connected successfully to MongoDB');
+    if (route === "/findOne") {
+        const filter = payload.filter || {};
+        const projection = payload.projection || {};
+        try{
+          const result = {
+            document: await collection.findOne(filter, projection)
+            };  
+            return res.status(200).send(result);
+        }
+        finally{
+          await client.close();
+        }
+    }
+    // else if(route === '/find'){
+    //   const aggQuery = [];
+    //   const filter = payload.filter || {};
+    //   const sort = payload.sort || {};
+    //   try {
+    //     const result = await collection.findOne(query);
+    //     console.log('Found document:', result);
+    //     return res.status(200).send(result);
+    //   }
+    //   finally{
+    //     await client.close();
+    //   }
+      
+    // }
+    else if(route === '/insertOne'){
+      const document = payload.document || {};
+      if (!document) {
+        return res.status(400).send('Document is required');
+      }
+      const result = await collection.insertOne(document);
+      console.log('Inserted document:', result.insertedId);
+      return res.status(200).send(result);
+    }
+    // else if(route === '/insertMany'){
+    //   const documents = payload.documents || [];
+    //   if (!documents.length) {
+    //     return res.status(400).send('Documents are required');
+    //   }
+
+    //   collection.updateOne({ name: 'John Doe' }, { $set: { name: 'Jane Doe' } });
+    // }
+    else if(route === '/vectorSearch'){
+      const query = payload.query || {};
+      if (!query) {
+        return res.status(400).send('Query is required');
+      }
+      try{
+        // const singleVector = await embeddings.embedQuery(query);
+        // console.log(singleVector);
+        const similaritySearchResults = await vectorStore.similaritySearch(
+          query,
+          1
+        );
+        for (const doc of similaritySearchResults) {
+          console.log(`* ${doc.pageContent} [${JSON.stringify(doc.metadata, null)}]`);
+        }
+
+        const aiMsg = await llm.invoke([
+          [
+            "system",
+            process.env.LLM_PROMPT,
+          ],
+          similaritySearchResults[0].pageContent,
+        ]);
+        
+        return res.status(200).send(aiMsg.content);
+        
+      }
+      finally{
+        await client.close();
+      }
+      
+    } 
+  }
+  catch (error) {
+// Handle the error here
+  console.error(error);
+  }
+  
+  
+})
